48ba522752b88496ea00e1825d9889309eac7ebe
anuff <anufshaikh30@gmail.com>
2026-01-18 22:22:57 +0530

    Added vans list and van detail pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Vans from './pages/Vans';
+import VanDetail from './pages/VanDetail';
+import './server';
 
 function App() {
   return (
@@ -17,6 +20,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
         </Routes>
       </BrowserRouter>
     </>
